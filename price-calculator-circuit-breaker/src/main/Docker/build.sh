9c53567d3bb2f80cb53dda6e49a4ee37f5b4c0e5
ganoush <ganeshatmail@gmail.com>
2017-03-20 01:33:31 -0400

    Update Docker Changes

--- price-calculator-circuit-breaker/src/main/Docker/build.sh
+++ price-calculator-circuit-breaker/src/main/Docker/build.sh
@@ -2,6 +2,6 @@
 
 rm -fr build
 mkdir build
-cp ../build/libs/sprice-calculator-circuit-breaker-0.0.1-SNAPSHOT.jar build
+cp ../build/libs/price-calculator-circuit-breaker-0.0.1-SNAPSHOT.jar build
 
-docker build -t sb_web .
+docker build -t ct_web .
